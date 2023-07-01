2f3359cd9bfcb6d361aa8268f0e9f77a15d9978b
Bradley Grainger <bgrainger@gmail.com>
2023-06-30 18:40:07 -0700

    Add reference output from Boost test suite.

--- tests/NGuid.Tests/GuidHelpersTests.cs
+++ tests/NGuid.Tests/GuidHelpersTests.cs
@@ -15,6 +15,7 @@ public class GuidHelpersTests
 
 	[Theory]
 	[InlineData("www.widgets.com", 3, "3d813cbb-47fb-32ba-91df-831e1593ac29" /*, TestDisplayName = "RFC 4122 Appendix B, as updated by http://www.rfc-editor.org/errata_search.php?rfc=4122" */)]
+	[InlineData("www.widgets.com", 5, "21f7f8de-8051-5b89-8680-0195ef798b6a" /*, TestDisplayName = "Boost Test Suite https://github.com/boostorg/uuid/blob/2bc0c8e71677f387afdc09bc4f8d609d2c74e80e/test/test_generators.cpp#L85C25-L85C121" */)]
 	[InlineData("python.org", 3, "6fa459ea-ee8a-3ca4-894e-db77e160355e" /*, TestDisplayName = "Python implementation (http://docs.python.org/library/uuid.html#uuid-example)" */)]
 	[InlineData("python.org", 5, "886313e1-3b8a-5372-9b90-0c9aee199e5d" /*, TestDisplayName = "Python implementation (http://docs.python.org/library/uuid.html#uuid-example)" */)]
 	public void CreateDeterministicDnsGuid(string name, int version, string expected) =>
