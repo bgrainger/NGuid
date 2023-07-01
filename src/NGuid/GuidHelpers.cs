59aed7d7a4fb07e2172d8f9192fd8defb9bd076c
Bradley Grainger <bgrainger@gmail.com>
2023-06-30 18:32:37 -0700

    Modernise code style.

--- src/NGuid/GuidHelpers.cs
+++ src/NGuid/GuidHelpers.cs
@@ -14,10 +14,8 @@ public static class GuidHelpers
 	/// <param name="namespaceId">The ID of the namespace.</param>
 	/// <param name="name">The name (within that namespace).</param>
 	/// <returns>A UUID derived from the namespace and name.</returns>
-	public static Guid Create(Guid namespaceId, string name)
-	{
-		return Create(namespaceId, name, 5);
-	}
+	public static Guid Create(Guid namespaceId, string name) =>
+		Create(namespaceId, name, 5);
 
 	/// <summary>
 	/// Creates a name-based UUID using the algorithm from RFC 4122 §4.3.
@@ -31,24 +29,28 @@ public static class GuidHelpers
 	public static Guid Create(Guid namespaceId, string name, int version)
 	{
 		// see https://github.com/LogosBible/Logos.Utility/blob/master/src/Logos.Utility/GuidUtility.cs and https://faithlife.codes/blog/2011/04/generating_a_deterministic_guid/ for the original version of this code
-		if (name == null)
+#if NET6_0_OR_GREATER
+		ArgumentNullException.ThrowIfNull(name);
+#else
+		if (name is null)
 			throw new ArgumentNullException(nameof(name));
-		if (version != 3 && version != 5)
+#endif
+		if (version is not (3 or 5))
 			throw new ArgumentOutOfRangeException(nameof(version), "version must be either 3 or 5.");
 
 		// convert the name to a sequence of octets (as defined by the standard or conventions of its namespace) (step 3)
 		// ASSUME: UTF-8 encoding is always appropriate
-		byte[] nameBytes = Encoding.UTF8.GetBytes(name);
+		var nameBytes = Encoding.UTF8.GetBytes(name);
 
 		// convert the namespace UUID to network order (step 3)
-		byte[] namespaceBytes = namespaceId.ToByteArray();
+		var namespaceBytes = namespaceId.ToByteArray();
 		SwapByteOrder(namespaceBytes);
 
 		// compute the hash of the name space ID concatenated with the name (step 4)
 		byte[] hash;
 #pragma warning disable CA5350 // Do Not Use Weak Cryptographic Algorithms
 #pragma warning disable CA5351 // Do Not Use Broken Cryptographic Algorithms
-		using (HashAlgorithm algorithm = version == 3 ? (HashAlgorithm) MD5.Create() : SHA1.Create())
+		using (var algorithm = version == 3 ? (HashAlgorithm) MD5.Create() : SHA1.Create())
 		{
 			algorithm.TransformBlock(namespaceBytes, 0, namespaceBytes.Length, null, 0);
 			algorithm.TransformFinalBlock(nameBytes, 0, nameBytes.Length);
@@ -58,7 +60,7 @@ public static class GuidHelpers
 #pragma warning restore CA5350 // Do Not Use Weak Cryptographic Algorithms
 
 		// most bytes from the hash are copied straight to the bytes of the new GUID (steps 5-7, 9, 11-12)
-		byte[] newGuid = new byte[16];
+		var newGuid = new byte[16];
 		Array.Copy(hash, 0, newGuid, 0, 16);
 
 		// set the four most significant bits (bits 12 through 15) of the time_hi_and_version field to the appropriate 4-bit version number from Section 4.1.3 (step 8)
@@ -75,17 +77,17 @@ public static class GuidHelpers
 	/// <summary>
 	/// The namespace for fully-qualified domain names (from RFC 4122, Appendix C).
 	/// </summary>
-	public static readonly Guid DnsNamespace = new Guid("6ba7b810-9dad-11d1-80b4-00c04fd430c8");
+	public static readonly Guid DnsNamespace = new("6ba7b810-9dad-11d1-80b4-00c04fd430c8");
 
 	/// <summary>
 	/// The namespace for URLs (from RFC 4122, Appendix C).
 	/// </summary>
-	public static readonly Guid UrlNamespace = new Guid("6ba7b811-9dad-11d1-80b4-00c04fd430c8");
+	public static readonly Guid UrlNamespace = new("6ba7b811-9dad-11d1-80b4-00c04fd430c8");
 
 	/// <summary>
 	/// The namespace for ISO OIDs (from RFC 4122, Appendix C).
 	/// </summary>
-	public static readonly Guid IsoOidNamespace = new Guid("6ba7b812-9dad-11d1-80b4-00c04fd430c8");
+	public static readonly Guid IsoOidNamespace = new("6ba7b812-9dad-11d1-80b4-00c04fd430c8");
 
 	// Converts a GUID (expressed as a byte array) to/from network order (MSB-first).
 	internal static void SwapByteOrder(byte[] guid)
@@ -98,7 +100,7 @@ public static class GuidHelpers
 
 	private static void SwapBytes(byte[] guid, int left, int right)
 	{
-		byte temp = guid[left];
+		var temp = guid[left];
 		guid[left] = guid[right];
 		guid[right] = temp;
 	}
--- tests/NGuid.Tests/GuidHelpersTests.cs
+++ tests/NGuid.Tests/GuidHelpersTests.cs
@@ -5,37 +5,20 @@ public class GuidHelpersTests
 	[Fact]
 	public void SwapByteOrder()
 	{
-		Guid guid = new Guid(0x01020304, 0x0506, 0x0708, 9, 10, 11, 12, 13, 14, 15, 16);
-		byte[] bytes = guid.ToByteArray();
+		var guid = new Guid(0x01020304, 0x0506, 0x0708, 9, 10, 11, 12, 13, 14, 15, 16);
+		var bytes = guid.ToByteArray();
 		Assert.Equal(new byte[] { 4, 3, 2, 1, 6, 5, 8, 7, 9, 10, 11, 12, 13, 14, 15, 16 }, bytes);
 
 		GuidHelpers.SwapByteOrder(bytes);
 		Assert.Equal(new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 }, bytes);
 	}
 
-	[Fact]
-	public void CreateVersion3FromWidgetsCom()
-	{
-		// run the test case from RFC 4122 Appendix B, as updated by http://www.rfc-editor.org/errata_search.php?rfc=4122
-		Guid guid = GuidHelpers.Create(GuidHelpers.DnsNamespace, "www.widgets.com", 3);
-		Assert.Equal(new Guid("3d813cbb-47fb-32ba-91df-831e1593ac29"), guid);
-	}
-
-	[Fact]
-	public void CreateVersion3FromPythonOrg()
-	{
-		// run the test case from the Python implementation (http://docs.python.org/library/uuid.html#uuid-example)
-		Guid guid = GuidHelpers.Create(GuidHelpers.DnsNamespace, "python.org", 3);
-		Assert.Equal(new Guid("6fa459ea-ee8a-3ca4-894e-db77e160355e"), guid);
-	}
-
-	[Fact]
-	public void CreateVersion5FromPythonOrg()
-	{
-		// run the test case from the Python implementation (http://docs.python.org/library/uuid.html#uuid-example)
-		Guid guid = GuidHelpers.Create(GuidHelpers.DnsNamespace, "python.org", 5);
-		Assert.Equal(new Guid("886313e1-3b8a-5372-9b90-0c9aee199e5d"), guid);
-	}
+	[Theory]
+	[InlineData("www.widgets.com", 3, "3d813cbb-47fb-32ba-91df-831e1593ac29" /*, TestDisplayName = "RFC 4122 Appendix B, as updated by http://www.rfc-editor.org/errata_search.php?rfc=4122" */)]
+	[InlineData("python.org", 3, "6fa459ea-ee8a-3ca4-894e-db77e160355e" /*, TestDisplayName = "Python implementation (http://docs.python.org/library/uuid.html#uuid-example)" */)]
+	[InlineData("python.org", 5, "886313e1-3b8a-5372-9b90-0c9aee199e5d" /*, TestDisplayName = "Python implementation (http://docs.python.org/library/uuid.html#uuid-example)" */)]
+	public void CreateDeterministicDnsGuid(string name, int version, string expected) =>
+		Assert.Equal(new Guid(expected), GuidHelpers.Create(GuidHelpers.DnsNamespace, name, version));
 
 	[Fact]
 	public void CreateNullName()
